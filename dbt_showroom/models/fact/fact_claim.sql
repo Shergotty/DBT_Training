a99fa6afaadd5dc4662770b00c8bcca0b8772987
Sebastian Hackler-Schürmann <sebastian.hackler@gmx.de>
2026-01-20 16:40:12 +0100

    update fact

--- dbt_showroom/models/fact/fact_claim.sql
+++ dbt_showroom/models/fact/fact_claim.sql
@@ -1,16 +1,14 @@
 {{ config(materialized='table', schema = 'mart') }}
 
 WITH FACT_CLAIM AS (
-    SELECT hash_claim,
-            claim_id,
-            policy_number,
-            incident_date,
-            claim_amount,
-            claim_type,
-            status,
-            hash_diff,
-            timestamp_insert
-    FROM public_staging.staging_contract as  stg
+    SELECT 
+        claim_id
+        , policy_number
+        , incident_date
+        , claim_amount
+        , ct.CLAIM_TYPE_PK
+        , cs.CLAIM_STATUS_PK
+    FROM {{ ref('staging_claim') }} as  stg
     INNER JOIN {{ ref('dim_claim_status') }} as cs
     ON cs.CLAIM_STATUS_PK = md5(trim(stg.status))
     INNER join {{ ref('dim_claim_type') }} as ct
--- dbt_showroom/models/fact/fact_contract.sql
+++ dbt_showroom/models/fact/fact_contract.sql
@@ -1,7 +1,8 @@
 {{ config(materialized='table', schema = 'mart') }}
 
 WITH FACT_CONCTRACT AS (
-    SELECT policy_number
+    SELECT 
+           policy_number
            , client_id
            , it.insurance_type_pk
            , monthly_premium
@@ -9,8 +10,7 @@ WITH FACT_CONCTRACT AS (
            , pp.payment_type_pk
            , acquisition_cost
            , insurance_sum
-           , timestamp_insert
-    FROM public_staging.staging_contract as  stg
+    FROM {{ ref('staging_contract') }} as  stg
     INNER JOIN {{ ref('dim_payment_plan') }} as pp
     ON pp.payment_type_pk = md5(trim(stg.payment_plan))
     INNER join {{ ref('dim_insurance_type') }} as it
