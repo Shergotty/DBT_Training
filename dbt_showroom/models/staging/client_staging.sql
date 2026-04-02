b708cefc81107f9b617997cd68b62e0a57b5cdba
Shergotty <sebastian.hackler@gmx.de>
2026-04-02 16:46:58 +0200

    initial commit

--- dbt_showroom/models/staging/client_staging.sql
+++ dbt_showroom/models/staging/client_staging.sql
@@ -1,14 +1,1 @@
-SELECT 
-    MD5(client_id::text) AS HASH_PK
-    ,MD5(CONCAT_WS('-', TRIM(first_name::text), TRIM(last_name::text), TRIM(date_of_birth::text), TRIM(email::text), TRIM(phone::text), TRIM(address::text), TRIM(zip_code::text), TRIM(city::text))) AS HASH_DIFF
-    ,client_id
-    ,first_name
-    ,last_name
-    ,date_of_birth
-    ,email
-    ,phone
-    ,address
-    ,zip_code
-    ,city
-FROM 
-   {{source('seed', 'client')}}
+{{ generate_staging_model('seed') }}
