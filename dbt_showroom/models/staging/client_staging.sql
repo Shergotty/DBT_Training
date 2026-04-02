88590d7585fbdd4c713e984f212971702a6cb15d
Shergotty <sebastian.hackler@gmx.de>
2026-04-02 15:35:49 +0200

    update concat mode

--- dbt_showroom/models/staging/client_staging.sql
+++ dbt_showroom/models/staging/client_staging.sql
@@ -1,6 +1,6 @@
 SELECT 
     MD5(client_id::text) AS HASH_PK
-    ,MD5(CONCAT(first_name::text, last_name::text, date_of_birth::text, email::text, phone::text, address::text, zip_code::text, city::text)) AS HASH_DIFF
+    ,MD5(CONCAT_WS('-', TRIM(first_name::text), TRIM(last_name::text), TRIM(date_of_birth::text), TRIM(email::text), TRIM(phone::text), TRIM(address::text), TRIM(zip_code::text), TRIM(city::text))) AS HASH_DIFF
     ,client_id
     ,first_name
     ,last_name
